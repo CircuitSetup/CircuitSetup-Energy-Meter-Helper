403444500860dbab69d26af4ab04bf856776e00d
jdeglavina <jdeg@sugarmanstudios.com>
2026-08-26 02:06:20 -0400

    test: update release diagnostics version

--- tests/test_websocket_api.py
+++ tests/test_websocket_api.py
@@ -3418,7 +3418,7 @@ def test_setup_uses_the_home_assistant_diagnostics_snapshot_for_the_panel() -> N
         assert connection.results[-1] == (
             1,
             {
-                "integration_version": "0.2.1",
+                "integration_version": "0.3.0",
                 "home_assistant_version": HA_VERSION,
                 "config_entry_version": 1,
                 "setup_state": "no_device",
